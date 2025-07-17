8515f6c9740a57ce1c0b1df99397ef948c317083
Harold Steven Cabrera González <harold665.hscg@gmail.com>
2025-07-17 02:11:49 -0600

    fixing imports

--- src/app/(routes)/(home)/components/TableData/colums.tsx
+++ src/app/(routes)/(home)/components/TableData/colums.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import type { Element } from "@prisma/client";
+import type { Element } from "../../../../../types/element";
 import { ColumnDef } from "@tanstack/react-table";
 import { Button } from "@/components/ui/button";
 import {
